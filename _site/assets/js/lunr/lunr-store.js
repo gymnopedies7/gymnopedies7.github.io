92ebd4a8ae4ba12bc089422545f57538d7f92060
Hwang <david@hwangminsuui-MacBook-Pro.local>
2020-08-21 17:19:46 +0900

    jeklly theme 적용
    블로그 첫 등록

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "머린이의 첫 게시글입니다.",
+        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["Edge Case"],
+        "tags": ["content","css","edge case","lists","markup"],
+        "url": "http://localhost:4000/edge%20case/first/",
+        "teaser": null
+      }]
